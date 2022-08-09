f350a6b6b1c5faa645663558c5b4d5fbec2b72be
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-08-09 04:56:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  bracketsConfig = bracketsConfig.map((el) => el.join(""));
+ 
+//   console.log(bracketsConfig);
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (str.includes(bracketsConfig[i])) {
+ 
+      str = str.replace(bracketsConfig[i], "");
+      i = -1;
+    }
+  }
+
+  if (str.length == 0) return true;
+  else return false;
 }
